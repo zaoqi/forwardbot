2d1b74ed2e82bceea37ca755b5f2c950121b0be1
zaoqi <zaomir@outlook.com>
2019-02-02 19:12:17 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -84,6 +84,7 @@ keep_coolq(){
     do
 	if killall -0 CQA.exe
 	then
+	    echo
 	else
 	    rm -fr "$forwardbot_var_root/lib/coolq/data/app/io.github.richardchien.coolqhttpapi"
 	    must mkdir -p "$forwardbot_var_root/lib/coolq/data/app/io.github.richardchien.coolqhttpapi/config"

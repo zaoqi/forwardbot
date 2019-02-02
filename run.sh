f878f6d81296d799cfd588dda5659e7272827d15
zaoqi <zaomir@outlook.com>
2019-02-02 16:06:38 +0800

    run.sh-WIP

---
+++ run.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+
+must(){
+    until "$@"
+    do
+        echo
+    done
+}
+
+must cd var
+forwardbot_var_root="$PWD"
+must mkdir -p bin
+must mkdir -p lib
+must mkdir -p status
+export PATH="$forwardbot_var_root/bin:$PATH:$forwardbot_var_root/bin"
+export LANG=zh_CN.UTF-8
+
+must cd "$forwardbot_var_root/status"
+if [ ! -f installed_coolq ]
+then
+    must cd "$forwardbot_var_root/bin"
+    rm -fr winetricks
+    must wget https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
+    must chmod +x winetricks
+    must ./winetricks msscript
+    must ./winetricks winhttp
+    must ./winetricks cjkfonts
+
+    must cd "$forwardbot_var_root/lib"
+    rm -fr coolq
+    must mkdir coolq
+    must cd coolq
+    must mkdir tmp
+    must cd tmp
+    must wget -O CQA.zip http://dlsec.cqp.me/cqa-tuling
+    must unzip CQA.zip
+    must mv *Air/* "$forwardbot_var_root/lib/coolq/"
+    must cd "$forwardbot_var_root/lib/coolq"
+    must rm -fr tmp
+
+    must touch "$forwardbot_var_root/status/installed_coolq"
+fi
+
+must cd "$forwardbot_var_root/status"
+if [ ! -f installed_coolq_httpapi ]
+then
+    must cd "$forwardbot_var_root/lib/coolq/app"
+    must wget https://github.com/richardchien/coolq-http-api/releases/download/v4.7.1/io.github.richardchien.coolqhttpapi.cpk
+    must touch "$forwardbot_var_root/status/installed_coolq_httpapi"
+fi
+
+must cd "$forwardbot_var_root/status"
+if [ ! -f installed_python ]
+then
+    must cd "$forwardbot_var_root"
+    must mkdir -p src/python
+    must cd "$forwardbot_var_root/src/python"
+    if [ ! -d Python-3.7.2 ] || [ -f Python-3.7.2.tgz ]
+    then
+	rm -fr *.tgz
+	must wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
+	must tar -xvzf Python-3.7.2.tgz
+	must rm Python-3.7.2.tgz
+    fi
+    must cd "$forwardbot_var_root/src/python/Python-3.7.2"
+    must ./configure --enable-optimizations --prefix="$forwardbot_var_root"
+    must make
+    must make install
+
+    must touch "$forwardbot_var_root/status/installed_python"
+fi
+
+
+echo WIP
